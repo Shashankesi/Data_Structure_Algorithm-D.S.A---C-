8b5c06a0def6d04a273670e40136f8acd0093a22
Shashank <shashankesi224@gmail.com>
2026-03-03 15:42:03 +0530

    // Please enter the commit message for your changes. Lines starting // with '#' will be ignored, and an empty message aborts the commit.

---
+++ Hashing/build.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class Node{
+public:
+    string key;
+    int val;
+    Node* next;
+
+    Node(string key,int val){
+        this->key = key;
+        this->val = val;
+        next = NULL;
+    }
+};
+
+class HashTable{
+    int totalSize;
+    int currSize;
+    Node** table;
+
+public:
+    HashTable(int size){
+        totalSize = size;
+        currSize = 0;
+
+        table = new Node*[totalSize];
+        for(int i = 0; i < totalSize; i++){
+            table[i] = NULL;
+        }
+    }
+};   
+
+int main(){
+    HashTable ht(6);
+}
