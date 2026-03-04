5fd7a0ad2d7cfc28731d7f4df09540cfb150c73e
Shashank <shashankesi224@gmail.com>
2026-03-04 16:06:18 +0530

    // This is a temporary file created by Git to hold the commit message for the current commit.

--- Hashing/build.cpp
+++ Hashing/build.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 class Node{
@@ -12,6 +13,12 @@ public:
         this->key = key;
         this->val = val;
         next = NULL;
+    }
+
+    ~Node(){
+        if(next!=NULL){
+            delete next;
+        }
     }
 };
 
@@ -26,11 +33,51 @@ public:
         currSize = 0;
 
         table = new Node*[totalSize];
-        for(int i = 0; i < totalSize; i++){
+
+        for(int i=0; i< totalSize; i++){
             table[i] = NULL;
         }
     }
-};   
+
+    int hashFunction(string key){
+        int idx = 0;
+
+        for(int i=0;i<key.size();i++){
+            idx = (idx + (key[i]*key[i])) % totalSize;
+        }
+
+        return idx;
+    }
+
+    void insert(string key, int val){
+        int idx = hashFunction(key);
+
+        Node* newNode = new Node(key,val);
+
+        newNode->next = table[idx];
+        table[idx] = newNode;
+
+        currSize++;
+    }
+
+    void rehash(){
+        Node** oldTable=table;
+        int oldSize=totalSize;
+
+        totalSize=2*totalSize;
+        for(int i=0;i<oldSize;i++){
+            Node* temp=oldTable[i];
+            while(temp!=NULL){
+                insert(temp->key,temp->val);
+                temp=temp->next;
+            }
+            if(oldTable[i]!=NULL){
+                delete oldTable[i];
+            }
+        }
+        delete[] oldTable;
+    }
+};
 
 int main(){
     HashTable ht(6);
