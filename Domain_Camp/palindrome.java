df20602ada74dad58178afce1af488907885e270
Shashankesi <shashankesi224@gmail.com>
2026-06-05 14:04:08 +0530

    palindrome.java code file added inside domain_camp

---
+++ Domain_Camp/palindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+@FunctionalInterface
+interface Palindrome {
+    boolean check(String str);
+}
+
+public class PalindromeLambda {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        // Lambda Function
+        Palindrome palindrome = s -> {
+            String reversed = new StringBuilder(s).reverse().toString();
+            return s.equalsIgnoreCase(reversed);
+        };
+
+        if (palindrome.check(str)) {
+            System.out.println(str + " is a Palindrome.");
+        } else {
+            System.out.println(str + " is not a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
